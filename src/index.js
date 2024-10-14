a108711cd782067482767b0664adcc87db3b7541
Insspirit <kovshiktanya@gmail.com>
2024-10-15 00:30:38 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 
 // You should implement your task here.
 
+// function towelSort (matrix) {
+//   if (!matrix || matrix.length === 0) {
+//     return [] //проверим пустой ли массив
+//   }
+  
+//   let result = []
+
+//   for (let i = 0; i < matrix.length; i++)  {
+//     if (i % 2 === 0) { // если четная строка
+//       result = result.concat(matrix[i])//добавляем эл в конец массива
+//     } else {
+//       result = result.concat(matrix[i].reverse()) // если нечетное, что развернуть
+//     }
+//   }
+//   return result
+// }
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix || matrix.length === 0) {
+    return [] //проверим пустой ли массив
+  }
+  
+  let result = []
+
+  for (let i = 0; i < matrix.length; i++)  {
+    if (i % 2 === 0) { // если четная строка
+      result = result.concat(matrix[i])//добавляем эл в конец массива
+    } else {
+      result = result.concat(matrix[i].reverse()) // если нечетное, что развернуть
+    }
+  }
+  return result
 }
